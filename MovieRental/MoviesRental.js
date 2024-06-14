43e060f227e5e2a3fc8bc79c4ec897858b3d9070
Sarthak_Shishodia <sarthakshishodia2004@gmail.com>
2024-06-14 22:22:54 +0530

    MovieRentalClone

---
+++ MovieRental/MoviesRental.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    speed:1000,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+  });
